cc0ed5a624f79c17262324c5dcdfcce1ff4df088
SABARI <sabarisekar777@gmail.com>
2025-08-22 10:16:15 +0530

    feat : Added Widget Session

---
+++ apps/widget/modules/ui/screens/widget-auth-screen.tsx
@@ -0,0 +1,112 @@
+import z from "zod";
+import { WidgetHeader } from "../components/widget-header";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@workspace/ui/components/form";
+import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
+import { useMutation } from "convex/react";
+import { api } from "../../../../../packages/backend/convex/_generated/api";
+import { platform } from "os";
+import { Doc } from "../../../../../packages/backend/convex/_generated/dataModel";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+});
+
+export const WidgetAuthScreen = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+  const orgId = "1234";
+  const createContactSession = useMutation(api.public.contactSessions.create);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    if(!orgId) return;
+    const metadata : Doc<"contactSession">["metadata"] ={
+      userAgent : navigator.userAgent,
+      language : navigator.language,
+      platform : navigator.platform,
+      timezone : Intl.DateTimeFormat().resolvedOptions().timeZone
+    };
+    const contactSessionId = await createContactSession({
+      ...data,
+      organizationId : orgId,
+      metadata
+
+    })
+    console.log(contactSessionId);
+  };
+  return (
+    <>
+      <WidgetHeader>
+        <div className="flex flex-col justify-between gap-y-3 px-4 py-8">
+          <p className="font-bold text-4xl tracking-tight text-primary-foreground">
+            Hi there! 👋🏼
+          </p>
+          <p className="text-xl text-primary-foreground/90 font-medium">
+            Let&apos;s get you started.
+          </p>
+        </div>
+      </WidgetHeader>
+      <Form {...form}>
+        <form
+          className="flex flex-1 flex-col gap-y-4 p-4"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="h-10 bg-background"
+                    placeholder="eg. John Doe"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="h-10 bg-background"
+                    placeholder="eg. johndoe@example.com"
+                    type="text"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={form.formState.isSubmitting}
+            size="lg"
+            type="submit"
+          >
+            Continue
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
--- packages/backend/convex/public/contactSessions.ts
+++ packages/backend/convex/public/contactSessions.ts
@@ -1,13 +1,12 @@
-import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
-import { platform } from "os";
+import { mutation } from "../_generated/server";
 
-export default defineSchema({
-  contactSession: defineTable({
+const SESSION_DURATION = 24 * 60 * 60 * 1000;
+export const create = mutation({
+  args: {
     name: v.string(),
     email: v.string(),
     organizationId: v.string(),
-    expiresAt: v.number(),
     metadata: v.optional(
       v.object({
         userAgent: v.optional(v.string()),
@@ -24,10 +23,17 @@ export default defineSchema({
         currentUrl: v.optional(v.string()),
       })
     ),
-  })
-  .index("by_organization_id", ["organizationId"])
-  .index("by_expires_at", ["expiresAt"]),
-  users: defineTable({
-    name: v.string(),
-  }),
+  },
+  handler : async (ctx, args) => {
+    const now = Date.now();
+    const expiresAt = now + SESSION_DURATION;
+    const contactSessionId = await ctx.db.insert("contactSession", {
+        name: args.name,
+        email: args.email,
+        organizationId: args.organizationId,
+        metadata: args.metadata,
+        expiresAt,
+      });
+    return contactSessionId;
+  }
 });
