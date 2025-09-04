f430b529f983756d8cf9e1dfa4de26f33d484179
SABARI <sabarisekar777@gmail.com>
2025-09-04 14:40:03 +0530

    fix : fixing the widget-cdn url and dashboard

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  devIndicators : false
+  devIndicators : false,
+  async redirects() {
+    return [
+      {
+        source : "/",
+        destination: "/conversations",
+        permanent: false
+      }
+    ]
+  }
 }
 
 export default nextConfig
